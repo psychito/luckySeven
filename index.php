3b6c7bacc190bc3045631a5e684a40bbc397da76
drPsychito <alexanderurbinac@gmail.com>
2017-08-29 12:24:17 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.protostar
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+/** @var JDocumentHtml $this */
+
+$app  = JFactory::getApplication();
+$user = JFactory::getUser();
+
+// Output as HTML5
+$this->setHtml5(true);
+JHtml::_('bootstrap.framework');
+JHtml::_('jquery.framework');
+JHtml::_('script', 'template.js', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'jui/html5.js', array('version' => 'auto', 'relative' => true, 'conditional' => 'lt IE 9'));
+JHtml::_('stylesheet', 'siete.css', array('version' => 'auto', 'relative' => true));
+JHtml::_('stylesheet', '//fonts.googleapis.com/css?family=Lato:300,400,900');
+$doc->addStyleSheet($this->baseurl . '/media/jui/css/bootstrap.min.css');
+$this->addStyleDeclaration("h1, h2, h3, h4, h5, h6, .site-title {	font-family: Lato, sans-serif;
+}");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<jdoc:include type="head" />
+</head>
+<body>
+
+<div class="completo">
+	<div class="container-fluid">
+	<header>
+		<div class="row-fluid topper">
+			<div class="span3">
+				<jdoc:include type="modules" name="lsev-fecha" style="none" />
+			</div>
+			<div class="span3 offset6">
+				<jdoc:include type="modules" name="lsev-buscador" style="none" />
+			</div>
+		</div>
+		<div class="row-fluid logocont">
+			<div class="span9">
+				<jdoc:include type="modules" name="lsev-pubtop" style="none" />
+			</div>
+			<div class="span3 logo">
+				<a class="brand pull-left" href="<?php echo $this->baseurl; ?>/">
+				   <img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/logo.png" alt="SIETE"/>
+			  </a>
+			</div>
+		</div>
+		<div class="row-fluid menucont">
+			<div class="span9 menu">
+				<nav class="navigation" role="navigation">
+					<div class="navbar pull-left">
+						<a class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="element-invisible"><?php echo JTEXT::_('TPL_LUCKYSEVEN_TOGGLE_MENU'); ?></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+					</div>
+					<div class="nav-collapse">
+						<jdoc:include type="modules" name="lsev-menu" style="none" />
+					</div>
+				</nav>
+			</div>
+			<div class="span3 redes">
+				<jdoc:include type="modules" name="lsev-redes" style="none" />
+			</div>
+		</div>
+		</header>
+		<?php $menu = &JSite::getMenu();
+       if ($menu->getActive() = $menu->getDefault()) {
+?>
+<section class="frontpagecont">
+<div class="row-fluid slidercont">
+	<div class="span12 slider">
+		<jdoc:include type="modules" name="lsev-slider" style="none" />
+	</div>
+</div>
+<div class="row-fluid markcont">
+	<div class="span4 mark1">
+		<jdoc:include type="modules" name="lsev-mark1" style="none" />
+	</div>
+	<div class="span4 mark2">
+		<jdoc:include type="modules" name="lsev-mark2" style="none" />
+	</div>
+	<div class="span4 mark3">
+		<jdoc:include type="modules" name="lsev-mark3" style="none" />
+	</div>
+</div>
+</section>
+<?php }else{ ?>
+	<section class="contenidocont">
+		<div class="row-fluid contenedor">
+			<div class="span9">
+				<div class="span12 conttopcont">
+					<jdoc:include type="modules" name="lsev-conttop" style="none" />
+				</div>
+				<div class="span12 bread">
+					<jdoc:include type="modules" name="lsev-breadcrumb" style="none" />
+				</div>
+				<div class="span12 contenido">
+					<jdoc:include type="component" />
+				</div>
+				<div class="span12 contbottomcont">
+					<jdoc:include type="modules" name="lsev-contbottom" style="none" />
+				</div>
+			</div>
+			<div class="span3 derecont">
+				<jdoc:include type="modules" name="lsev-derecha" style="none" />
+			</div>
+		</div>
+	</section>
+<?php } ?>
+			<footer>
+			<div class="row-fluid footpubcont">
+				<div class="span12 footpub">
+					<jdoc:include type="modules" name="lsev-footpub" style="none" />
+				</div>
+			</div>
+			<div class="row-fluid footcont">
+				<div class="span5 foot1">
+					<jdoc:include type="modules" name="lsev-foot1" style="none" />
+				</div>
+				<div class="span5 foot2">
+					<jdoc:include type="modules" name="lsev-foot2" style="none" />
+				</div>
+				<div class="span2 foot3">
+					<jdoc:include type="modules" name="lsev-foot3" style="none" />
+				</div>
+			</div>
+			<p class="pull-right">
+				<a href="#top" id="back-top">
+					<?php echo JText::_('TPL_LUCKYSEVEN_BACKTOTOP'); ?>
+				</a>
+			</p>
+			<p>
+				&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
+			</p>
+		</footer>
+		<jdoc:include type="modules" name="debug" style="none" />
+	</div>
+</div>
+
+
+</body>
+</html>
